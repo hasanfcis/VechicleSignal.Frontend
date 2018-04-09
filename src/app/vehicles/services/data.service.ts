45aac84a53dc0509aa7f6be7a35c1899c73ad260
hasanfcis <hassnyoussef.fcis@gmail.com>
2018-04-09 19:37:50 +0200

    dataservice update

--- src/app/vehicles/services/data.service.ts
+++ src/app/vehicles/services/data.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
+import { promise } from "protractor";
+
+
 
 interface IServiceResponse {
     results: any[];
@@ -14,28 +17,17 @@ export class DataService {
     constructor(private http: HttpClient) {
     }
 
+    /** GET vehicles from the server */
+  public GetVehicles (): Observable<any[]> {
+    return this.http.get<any[]>("api/Vehicle/GetVehicles")
+  }
+
     public getData(): Promise<any> {
         return new Promise((resolve, reject) => {
-            this.http.get("http://localhost:4263/api/Vehicle/GetVehicles")
+            this.http.get("api/Vehicle/GetVehicles")
                 .subscribe((data: IServiceResponse) => {
                     resolve(data.results);
                 });
         });
     }
-    // TODO
-
-    // this.dataService.getData().then((json: any) => {
-    //     let idx = 0;
-    //     const data = [];
-
-    //     for (const i of data) {
-    //       i.CountryFlag = i.CountryFlag.toLocaleLowerCase();
-    //       i.Avatar = json[idx].picture.large;
-    //       i.Name = json[idx].name.first + ' ' + json[idx].name.last;
-    //       idx++;
-    //       data.push(i);
-    //     }
-
-    //     this.localData = data;
-    //   });
 }
--- src/app/vehicles/vehicles.component.ts
+++ src/app/vehicles/vehicles.component.ts
@@ -11,7 +11,6 @@ import {
 
 import { IgxGridComponent } from "igniteui-angular/grid/grid.component";
 import { STRING_FILTERS } from "igniteui-angular/main";
-// import { athletesData } from "./Services/data";
 import { DataService } from "./Services/data.service";
 import { athletesData } from "./Services/data";
 
@@ -25,6 +24,7 @@ export class VehiclesComponent implements OnInit, OnDestroy {
 
   @ViewChild("vehicleGrid", { read: IgxGridComponent })
   public vehicleGrid: IgxGridComponent;
+
 
   public localData: any[];
   public isFinished = false;
@@ -45,76 +45,20 @@ export class VehiclesComponent implements OnInit, OnDestroy {
     }
   }
 
-  get hideAthleteNumber() {
-    return this.windowWidth && this.windowWidth < 960;
-  }
-  get hideBeatsPerMinute() {
-    return this.windowWidth && this.windowWidth < 860;
-  }
-
   constructor(private zone: NgZone, private dataService: DataService) { }
 
   public ngOnInit() {
-    // this.localData = athletesData;
-    this.dataService.getData().then((json: any) => {
-        let idx = 0;
-        const data = [];
-
-        for (const i of data) {
-          i.Id = json[idx].Id;
-          i.CustomerName = json[idx].CustomerName;
-          i.StatusName = json[idx].StatusName;
-          i.VehicleIdentifier=json[idx].VehicleIdentifier;
-          i.RegisterNumber=json[idx].RegisterNumber;
-          i.LastSignalTime=json[idx].LastSignalTime;
-          i.Status=json[idx].StatusId;
-          idx++;
-          data.push(i);
-        }
-
-        this.localData = data;
-      });
+    this.dataService.GetVehicles().subscribe((json: any[]) => {
+      this.localData = json;
+    });
 
     this.windowWidth = window.innerWidth;
     this._timer = setInterval(() => this.ticker(), 3000);
   }
 
-
   public ngOnDestroy() {
     clearInterval(this._timer);
   }
-
-  public isTop3(cell): boolean {
-    const top = cell.value > 0 && cell.value < 4;
-    if (top) {
-      cell.row.nativeElement.classList.add("top3");
-    } else {
-      cell.row.nativeElement.classList.remove("top3");
-    }
-    return top;
-  }
-
-  public getIconType(cell) {
-    switch (cell.row.rowData.Position) {
-      case "up":
-        return "arrow_upward";
-      case "current":
-        return "arrow_forward";
-      case "down":
-        return "arrow_downward";
-    }
-  }
-
-  // public getBadgeType(cell) {
-  //   switch (cell.row.rowData.Position) {
-  //     case "up":
-  //       return "success";
-  //     case "current":
-  //       return "warning";
-  //     case "down":
-  //       return "error";
-  //   }
-  // }
 
   @HostListener("window:resize", ["$event"])
   public onResize(event) {
@@ -122,16 +66,16 @@ export class VehiclesComponent implements OnInit, OnDestroy {
   }
 
   public customerFilter(term) {
-    this.vehicleGrid.filter("CustomerName", term, STRING_FILTERS.contains);
+    this.vehicleGrid.filter("customerName", term, STRING_FILTERS.contains);
   }
 
   public statusFilter(term) {
-    this.vehicleGrid.filter("StatusName", term, STRING_FILTERS.startsWith);
+    this.vehicleGrid.filter("statusName", term, STRING_FILTERS.startsWith);
   }
 
   private ticker() {
     this.zone.runOutsideAngular(() => {
-      this.updateData();
+      this.populateGrid();
       this.zone.run(() => this.vehicleGrid.markForCheck());
     });
   }
@@ -140,33 +84,10 @@ export class VehiclesComponent implements OnInit, OnDestroy {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  private updateData() {
-   // alert('updated');
-    // this.localData.map((rec) => {
-    //   let val = this.generateRandomNumber(-1, 1);
-    //   switch (val) {
-    //     case -1:
-    //       rec.Position = "down";
-    //       val = 0;
-    //       break;
-    //     case 0:
-    //       rec.Position = "current";
-    //       val = 1;
-    //       break;
-    //     case 1:
-    //       rec.Position = "up";
-    //       val = 3;
-    //       break;
-    //   }
-    //   rec.TrackProgress += val;
-    // });
+  private populateGrid() {
 
-    // this.localData.sort((a, b) => b.TrackProgress - a.TrackProgress).map((rec, idx) => rec.Id = idx + 1);
-    // this.localData = this.localData.slice(0);
-
-    // if (this.localData[0].TrackProgress >= 100) {
-    //   this.live = false;
-    //   this.isFinished = true;
-    // }
+    this.dataService.GetVehicles().subscribe((json: any[]) => {
+      this.localData = json;
+    });
   }
 }
